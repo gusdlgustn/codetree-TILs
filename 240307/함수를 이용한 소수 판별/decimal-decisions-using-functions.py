4eb4d3a89c181e9f03d08db1283689d55bbcc230
waffle_pan <110707076+gusdlgustn@users.noreply.github.com>
2024-03-07 09:55:24 +0900

    Title: 함수를 이용한 소수 판별, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240307/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240307/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -2,6 +2,9 @@ a, b = map(int, input().split())
 lst = [] # 소수 담을 리스트
 
 def is_prime(n):
+    if n == 1:
+        return False
+
     for i in range(2, n):
         if n%i == 0:
             return False
