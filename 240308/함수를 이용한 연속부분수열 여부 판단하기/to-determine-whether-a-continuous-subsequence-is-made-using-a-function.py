f93545b7ef739a2f1b779eea3faa265fdc510881
waffle_pan <110707076+gusdlgustn@users.noreply.github.com>
2024-03-08 15:29:49 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240308/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240308/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -4,7 +4,7 @@ B = list(map(int, input().split()))
 
 
 def is_subseq(A, B, n1, n2):
-    for i in range(n1-n2):
+    for i in range(n1-n2+1):
         sub_A = A[0+i:n2+i]
         if sub_A == B:
             return True
