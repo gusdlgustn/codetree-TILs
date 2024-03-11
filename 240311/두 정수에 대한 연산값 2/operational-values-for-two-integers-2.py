4f47c6085335b2780b3060c39b0b3735b6f3a1a3
waffle_pan <110707076+gusdlgustn@users.noreply.github.com>
2024-03-11 11:25:11 +0900

    Title: 두 정수에 대한 연산값 2, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240311/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -0,0 +1,14 @@
+a, b = map(int, input().split())
+
+def change(a, b):
+    if a<b:
+        a += 10
+        b *= 2
+    else:
+        a *= 2
+        b += 10
+    return a ,b
+
+
+a, b = change(a, b)
+print(a, b)
