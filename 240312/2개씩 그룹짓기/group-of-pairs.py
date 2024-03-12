9071c994308370ef6c07725e28bd0d0f0d1e5b6e
waffle_pan <110707076+gusdlgustn@users.noreply.github.com>
2024-03-12 09:43:47 +0900

    Title: 2개씩 그룹짓기, Time: 155ms, Memory: 25MB, Status: Passed - CodeTree

--- 240312/2개씩 그룹짓기/group-of-pairs.py
+++ 240312/2개씩 그룹짓기/group-of-pairs.py
@@ -2,10 +2,9 @@ N = int(input())
 arr = list(map(int, input().split()))
 
 arr.sort()
-summ = 0
 
+maxx=0
 for i in range(N):
-    maxx=0
     summ = arr[i]+arr[2*N-1-i]
     if summ>maxx:
         maxx = summ
