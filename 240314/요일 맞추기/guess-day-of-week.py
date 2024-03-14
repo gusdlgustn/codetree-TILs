066f1e1e677f2f3cc3796e8680f22f8b7145a6c3
waffle_pan <110707076+gusdlgustn@users.noreply.github.com>
2024-03-14 13:20:52 +0900

    Title: 요일 맞추기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240314/요일 맞추기/guess-day-of-week.py
+++ 240314/요일 맞추기/guess-day-of-week.py
@@ -4,12 +4,12 @@ days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 day1 = 0
-for i in range(m1+1):
+for i in range(m1):
     day1 += num_of_days[i]
 day1 += d1
 
 day2 = 0
-for i in range(m2+1):
+for i in range(m2):
     day2 += num_of_days[i]
 day2 += d2
 
