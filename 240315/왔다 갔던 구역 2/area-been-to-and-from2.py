ba95c52fb133b14ff29c4338cf7516bb5aa52043
waffle_pan <110707076+gusdlgustn@users.noreply.github.com>
2024-03-15 20:49:53 +0900

    Title: 왔다 갔던 구역 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240315/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,33 @@
+n = int(input())
+
+order = []
+for i in range(n):
+    x, direc = input().split()
+    x = int(x)
+    if direc == 'L':
+        x = x*(-1)
+    order.append(x)
+
+
+sum_L = 0
+sum_R = 0
+for elem in order:
+    if elem < 0:
+        sum_L += elem*(-1)
+    else:
+        sum_R += elem
+
+
+section = [0]*(sum_L + sum_R + 2) # 수평선 범위
+current = 0 + sum_L # 0(시작위치) + offset
+
+for moving in order:
+    if moving > 0:
+        for i in range(current+1, moving+1):
+            section[i] += 1
+    else: # moving < 0
+        for i in range(current+moving, current):
+            section[i] += 1
+
+overlap = [i for elem in section if elem > 1]
+print(len(overlap))
