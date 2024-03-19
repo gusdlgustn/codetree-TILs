5d252b03e39e3dcdf1bd684f8f9edba3f7ec7cf0
waffle_pan <110707076+gusdlgustn@users.noreply.github.com>
2024-03-19 14:04:32 +0900

    Title: 흰검 칠하기, Time: 202ms, Memory: 38MB, Status: Passed - CodeTree

---
+++ 240319/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,38 @@
+n = int(input())
+
+move = []
+
+for _ in range(n):
+    distance, direction = input().split()
+    distance = int(distance)
+    move.append((distance, direction))
+
+offset = 100000 # 100 * 1000
+
+current = 100000 # 현재 위치(0) + offset
+
+tile = [[''] for _ in range(200001)]
+
+for distance, direction in move:
+    if direction == 'L': # 흰색
+        for i in range(current-distance+1, current+1):
+            tile[i] += ['W'] # 흰색 추가
+        current -= (distance-1)
+    else: # direction == 'R' 검은색
+        for i in range(current, current+distance):
+            tile[i] += ['B'] # 흰색 추가
+        current += (distance-1)
+
+W = 0 # 흰색
+B = 0 # 검은색
+G = 0 # 회색
+
+for elem in tile:
+    if (elem.count('W') >= 2) and (elem.count('B') >= 2):
+        G += 1
+    elif elem[-1] == 'W':
+        W += 1
+    elif elem[-1] == 'B':
+        B += 1
+
+print(W, B, G)
