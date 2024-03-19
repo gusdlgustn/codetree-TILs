a30fed8eb4854257beeaa2b7ce7bf44156ce60ed
waffle_pan <110707076+gusdlgustn@users.noreply.github.com>
2024-03-19 16:57:04 +0900

    Title: 연속되는 수 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240319/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,12 @@
+N = int(input())
+lst = []
+for _ in range(N):
+    num = int(input())
+    lst.append(num)
+
+cnt = 0
+for i in range(N):
+    if (i == 0) or (lst[i] != lst[i - 1]):
+        cnt += 1
+
+print(cnt)
