93ede7777cd650668fbd76f4d09bfcd5fbf7a94c
waffle_pan <110707076+gusdlgustn@users.noreply.github.com>
2024-03-20 13:46:30 +0900

    Title: 벌금은 누구에게, Time: 147ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240320/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,17 @@
+N, M, K = map(int, input().split())
+
+students = [i for i in range(0, N+1)] # 1~N번 학생 번호
+students[0] = -1 # 0번 학생은 없다고 가정
+penalty = [ # 벌칙 받는 학생의 번호
+    int(input())
+    for _ in range(M)
+]
+
+for p in penalty:
+    students[p] -= 1
+    if 0 in students:
+        print(students.index(0))
+        break
+
+if not 0 in students:
+    print(-1)
