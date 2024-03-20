6a12fca86de3e8d7fefa23caed0309803886acfc
waffle_pan <110707076+gusdlgustn@users.noreply.github.com>
2024-03-20 12:57:45 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240320/연속되는 수 2/continuous-number2.py
+++ 240320/연속되는 수 2/continuous-number2.py
@@ -5,12 +5,15 @@ for _ in range(N):
     num = int(input())
     lst.append(num)
 
-ans, cnt = 0, 1
-for i in range(N-1):
-    if i >= 1 and (lst[i] == lst[i+1]):
-        cnt += 1
-    else:
-        cnt = 1
-    
-    ans = max(ans, cnt)
+ans, cnt = 0, 0
+for i in range(n):
+	# Case 1
+	if i >= 1 and arr[i] == arr[i - 1]:
+		cnt += 1
+	# Case 2
+	else:
+		cnt = 1
+	
+	ans = max(ans, cnt)
+
 print(ans)
