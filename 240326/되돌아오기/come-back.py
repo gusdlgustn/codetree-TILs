f834e859036368b6d343e24a03ded4d1b2ede5e7
waffle_pan <110707076+gusdlgustn@users.noreply.github.com>
2024-03-26 16:19:27 +0900

    Title: 되돌아오기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240326/되돌아오기/come-back.py
@@ -0,0 +1,31 @@
+N = int(input())
+arr = []
+for _ in range(N):
+    direc, dist = input().split()
+    dist = int(dist)
+    arr.append((direc, dist))
+
+x, y = 0, 0
+move_dir = 0
+    #  동 남 서 북
+dxs = [1, 0, -1, 0]
+dys = [0, -1, 0, 1]
+
+mapper = {
+    'E':0,
+    'S':1,
+    'W':2,
+    'N':3
+}
+
+time = 0
+for direc, dist in arr:
+    move_dir = mapper[direc]
+    for t in range(dist):
+        nx, ny = x + dxs[move_dir], y + dys[move_dir]
+        x, y = nx, ny
+        time += 1
+
+        if x == 0 and y == 0:
+            print(time)
+            break
