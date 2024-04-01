62acddbb2f3e5b5272cd8ec21f8b5c17ee1782b9
waffle_pan <110707076+gusdlgustn@users.noreply.github.com>
2024-04-01 12:42:27 +0900

    Title: 일렬로 서있는 소 2, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240401/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240401/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -1,20 +1,13 @@
-const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split('\n');
+# 변수 선언 및 입력
+n = int(input())
+arr = list(map(int, input().split()))
 
-// 변수 선언 및 입력
-const n = Number(input[0]);
-const arr = input[1].trim().split(' ').map(Number);
+# 모든 쌍을 다 잡아봅니다.
+cnt = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            if arr[i] <= arr[j] and arr[j] <= arr[k]:
+                cnt += 1
 
-// 모든 쌍을 다 잡아봅니다.
-let cnt = 0;
-for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-        for (let k = j + 1; k < n; k++) {
-            if (arr[i] <= arr[j] && arr[j] <= arr[k]) {
-                cnt += 1;
-            }
-        }
-    }
-}
-
-console.log(cnt);
+print(cnt)
