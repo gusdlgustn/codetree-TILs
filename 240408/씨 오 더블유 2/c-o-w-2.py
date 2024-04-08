abac890eef8c996abc88a351fe06baeb67ade55f
waffle_pan <110707076+gusdlgustn@users.noreply.github.com>
2024-04-08 23:13:43 +0900

    Title: 씨 오 더블유 2, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240408/씨 오 더블유 2/c-o-w-2.py
+++ 240408/씨 오 더블유 2/c-o-w-2.py
@@ -7,7 +7,7 @@ cnt = 0
 for i in range(n):
     for j in range(i + 1, n):
             for k in range(j + 1, n):
-                        if string[i] == 'C' and string[j] == 'O' and string[k] == 'W':
-                                        cnt += 1
+                if string[i] == 'C' and string[j] == 'O' and string[k] == 'W':
+                    cnt += 1
 
-                                        print(cnt)
+print(cnt)
