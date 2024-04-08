4bd2b3fa8aebc5e62c06300398f7d9e3432552df
waffle_pan <110707076+gusdlgustn@users.noreply.github.com>
2024-04-08 23:12:50 +0900

    Title: 씨 오 더블유 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240408/씨 오 더블유 2/c-o-w-2.py
@@ -0,0 +1,13 @@
+# 변수 선언 및 입력
+n = int(input())
+string = input()
+
+# 모든 쌍을 다 잡아봅니다.
+cnt = 0
+for i in range(n):
+    for j in range(i + 1, n):
+            for k in range(j + 1, n):
+                        if string[i] == 'C' and string[j] == 'O' and string[k] == 'W':
+                                        cnt += 1
+
+                                        print(cnt)
